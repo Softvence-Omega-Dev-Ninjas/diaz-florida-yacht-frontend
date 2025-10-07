347e2634dc7604545111061bf423315fa44f2c91
Arnab Gupta <arnabgupta1111@gmail.com>
2025-10-08 02:29:04 +0600

    add theme variable

--- src/app/(main-layout)/(home)/page.tsx
+++ src/app/(main-layout)/(home)/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  return <div className="text-primary bg-secondary">HomePage</div>;
+};
 
-export default HomePage
+export default HomePage;
