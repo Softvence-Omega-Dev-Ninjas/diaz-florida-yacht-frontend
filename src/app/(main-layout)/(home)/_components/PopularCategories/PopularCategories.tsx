a7b495d4399ec17695c9a56b0444a030c37dd67e
habibb2r <habibhk127@gmail.com>
2025-10-09 04:14:05 +0600

    Fix: Popular Categories

--- src/app/(main-layout)/(home)/_components/PopularCategories/PopularCategories.tsx
+++ src/app/(main-layout)/(home)/_components/PopularCategories/PopularCategories.tsx
@@ -6,7 +6,7 @@ import CategoryCard from './CategoryCard';
 const PopularCategories = () => {
     return (
         <CustomContainer>
-            <h2 className='text-5xl text-center font-bold'>Browse From Popular Categories</h2>
+            <h2 className='text-3xl sm:text-4xl lg:text-5xl text-center font-bold'>Browse From Popular Categories</h2>
             <div className="flex flex-wrap justify-center items-center gap-10 my-10">
                 {
                     democategoriesdata.map((category) => <CategoryCard key={category.id} category={category} />)
